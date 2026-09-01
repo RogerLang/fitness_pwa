01890b33b0ef5c1570a5dbf51baeaa6ecd3b2cbb
RogerLang <46833679+RogerLang@users.noreply.github.com>
2026-09-01 23:42:40 +0800

    Update cache version from v15 to v16

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "fitness-pwa-v15";
+const CACHE = "fitness-pwa-v16";
 const ASSETS = ["./","./index.html","./styles.css","./training-ux-v13.css","./training-ux-v14.css","./performance-v16.css","./app.js","./v2.js","./v2-fix.js","./v3-ui.js","./sync-v4.js","./local-v7.js","./today-sync-v12.js","./training-ux-v13.js","./training-ux-v14.js","./recovery-v15.js","./performance-v16.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
