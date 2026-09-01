7a64f6e70a5ff20163711ea20d8d169ae252fe59
RogerLang <46833679+RogerLang@users.noreply.github.com>
2026-09-01 19:20:08 +0800

    Bump PWA cache for v2 integration fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "fitness-pwa-v2";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./v2.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE = "fitness-pwa-v3";
+const ASSETS = ["./","./index.html","./styles.css","./app.js","./v2.js","./v2-fix.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener("fetch", event => {
