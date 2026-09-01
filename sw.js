55f9ff6f49b05b97530753236e9788acbcc3329a
RogerLang <46833679+RogerLang@users.noreply.github.com>
2026-09-01 22:22:52 +0800

    v13: cache training context UX

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "fitness-pwa-v11";
-const ASSETS = ["./","./index.html","./styles.css","./app.js","./v2.js","./v2-fix.js","./v3-ui.js","./sync-v4.js","./local-v7.js","./today-sync-v12.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE = "fitness-pwa-v12";
+const ASSETS = ["./","./index.html","./styles.css","./training-ux-v13.css","./app.js","./v2.js","./v2-fix.js","./v3-ui.js","./sync-v4.js","./local-v7.js","./today-sync-v12.js","./training-ux-v13.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener("fetch", event => {
