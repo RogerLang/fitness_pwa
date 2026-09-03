f18625ac320d1baeee6ee712d6428dec77e38f8c
RogerLang <46833679+RogerLang@users.noreply.github.com>
2026-09-03 18:08:40 +0800

    v48 bump training snap cache

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const SHELL_CACHE = "fitness-pwa-shell-v47";
+const SHELL_CACHE = "fitness-pwa-shell-v48";
 const SHELL_ASSETS = [
   "./",
   "./index.html",
   "./rescue.html",
   "./styles.css?v=39",
-  "./training-motion.css?v=46",
+  "./training-motion.css?v=48",
   "./app.js?v=40",
   "./training.js",
-  "./training-motion.js?v=47",
+  "./training-motion.js?v=48",
   "./sync.js",
   "./manifest.webmanifest",
   "./icon-192.png",
